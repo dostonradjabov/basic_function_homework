1acfd52d3e45dde3ff40bd739abce8d21be7d55c
dostonradjabov <radjabovdoston0809@gmail.com>
2023-10-05 19:51:42 +0500

    code academy

--- basic_func09.py
+++ basic_func09.py
@@ -1,5 +1,8 @@
 # Create a function called main.
-
+def main():
+    a = 5
+    a = a-1
+    return int(a)
 # Create function arguments a.
-
+print(main())
 # decrease the value of a to one and return
