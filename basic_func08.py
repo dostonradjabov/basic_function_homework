ca6a2d6ad68e6e4e502e169d1c2c73d236ab8657
dostonradjabov <radjabovdoston0809@gmail.com>
2023-10-05 19:49:20 +0500

    code academy

--- basic_func08.py
+++ basic_func08.py
@@ -1,5 +1,8 @@
 # Create a function called main.
-
+def main():
+    a = 8
+    a = a+1
 # Create function arguments a.
-
+    return int(a)
 # Increase the value of a to one and return.
+print(main())
